3e208d3ebc68343f486840a7657837d19d70ad1b
김승진 <klli1naver@gmail.com>
2022-01-13 16:35:42 +0900

    created:: model.home

---
+++ server/model/home.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, TIMESTAMP, text
+
+from server.model import Base
+from server.model import user
+
+
+class Home(Base):
+    __talbname__ = 'home'
+
+    idhoem = Column(Integer, primary_key=True)
+    title = Column(VARCHAR(50), nullable=True)
+    content = Column(VARCHAR(2000), nullable=True)
+    photo = Column(VARCHAR(255))
+    kategorie = Column(VARCHAR(45), nullable=True)
+    town = Column(VARCHAR(45), nullable=True)
+    price = Column(VARCHAR(100), nullable=True)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
+    user_iduser = Column(Integer, ForeignKey(user.iduser))
+
+
