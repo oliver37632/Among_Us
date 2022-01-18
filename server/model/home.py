ca48fd7568a41e3f4ddf338d5d2a7a3bfcb5f5cb
김승진 <klli1naver@gmail.com>
2022-01-18 21:01:09 +0900

    created:: model.home

--- server/model/home.py
+++ server/model/home.py
@@ -7,11 +7,11 @@ from server.model import user
 class Home(Base):
     __talbname__ = 'home'
 
-    idhoem = Column(Integer, primary_key=True)
+    idhoem = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(VARCHAR(50), nullable=True)
     content = Column(VARCHAR(2000), nullable=True)
-    photo = Column(VARCHAR(255))
-    kategorie = Column(VARCHAR(45), nullable=True)
+    image = Column(VARCHAR(255))
+    category = Column(VARCHAR(45), nullable=True)
     town = Column(VARCHAR(45), nullable=True)
     price = Column(VARCHAR(100), nullable=True)
     created_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
