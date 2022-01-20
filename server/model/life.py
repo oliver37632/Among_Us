169d0a124a220c6d18efa8d2971b5fe6557cef71
김승진 <klli1naver@gmail.com>
2022-01-21 02:39:50 +0900

    fix:: model.life

--- server/model/life.py
+++ server/model/life.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, TIMESTAMP, text
 
 from server.model import Base
-from server.model import user
 
 
 class Life(Base):
-    __talbname__ = 'life'
+    __tablename__  = 'life'
 
     idlife = Column(Integer, primary_key=True, autoincrement=True)
     content = Column(VARCHAR(2000), nullable=True)
@@ -13,6 +12,6 @@ class Life(Base):
     category = Column(VARCHAR(45), nullable=True)
     town = Column(VARCHAR(45), nullable=True)
     created_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    user_iduser = Column(Integer, ForeignKey(user.iduser))
+    user_iduser = Column(Integer, ForeignKey('user.iduser'))
 
 
