6a9c56e29e81d0d7aec5c9315e7aaeff74427cc0
김승진 <klli1naver@gmail.com>
2022-01-21 02:40:00 +0900

    fix:: model.user

--- server/model/user.py
+++ server/model/user.py
@@ -2,15 +2,16 @@ from sqlalchemy import Column, Integer, VARCHAR
 from sqlalchemy.orm import relationship
 
 from server.model import Base
-
+from server.model.home import Home
+from server.model.life import Life
 
 class User(Base):
     __tablename__ = 'user'
 
-    iduser = Column(Integer, primary_key=True)
+    iduser = Column(Integer, primary_key=True, autoincrement=True)
     nickname = Column(VARCHAR(16), nullable=True)
     password = Column(VARCHAR(100), nullable=True)
     email = Column(VARCHAR(320), nullable=True)
+    life = relationship("Life", cascade="all,delete", backref="user")
+    home = relationship("Home", cascade="all,delete", backref="user")
 
-    home = relationship("home", cascade="all, delete", backref="user")
-    life = relationship("life", canonical="all, delete", backref="user")
