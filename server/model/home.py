a7f6385476adaa4e4e26fbaefa5bfae97d5350c8
김승진 <klli1naver@gmail.com>
2022-02-05 19:17:29 +0900

    fix:: model.home

--- server/model/home.py
+++ server/model/home.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, TIMESTAMP, text
+from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, DATETIME, text
 
 from server.model import Base
 
@@ -6,14 +6,14 @@ from server.model import Base
 class Home(Base):
     __tablename__  = 'home'
 
-    idhoem = Column(Integer, primary_key=True, autoincrement=True)
+    Id_home = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(VARCHAR(50), nullable=True)
     content = Column(VARCHAR(2000), nullable=True)
     image = Column(VARCHAR(255))
     category = Column(VARCHAR(45), nullable=True)
     town = Column(VARCHAR(45), nullable=True)
     price = Column(VARCHAR(100), nullable=True)
-    created_at = Column(TIMESTAMP, nullable=False, server_default=text("CURRENT_TIMESTAMP"))
-    user_iduser = Column(Integer, ForeignKey('user.iduser'))
+    created_at = Column(DATETIME, nullable=True, server_default=text("CURRENT_TIMESTAMP"))
+    nickname = Column(VARCHAR(16), ForeignKey('user.nickname'))
 
 
