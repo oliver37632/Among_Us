01100d2a29769417b577a7273524905e2bf87c14
김승진 <klli1naver@gmail.com>
2022-02-05 19:17:04 +0900

    fix:: controller.home

--- server/controller/home.py
+++ server/controller/home.py
@@ -9,19 +9,20 @@ from server.config import AWS_S3_BUCKET_NAME
 s3 = s3_connection()
 
 
-def home(title, content, town, price, nickname):
+def home(title, content, town, price, nickname, category):
     with session_scope() as session:
         new_home = Home(
             title=title,
             content=content,
             town=town,
             price=price,
-            nickname=nickname
+            nickname=nickname,
+            category=category
         )
         session.add(new_home)
-        session.commit(new_home)
+        session.commit()
 
-        name = new_home.idhoem
+        name = str(new_home.Id_home)
         s3_put_object(s3, AWS_S3_BUCKET_NAME, "./temp", name)
         location = s3.get_bucket_location(Bucket=AWS_S3_BUCKET_NAME)['LocationConstraint']
         image_url = f'https://{AWS_S3_BUCKET_NAME}.s3.{location}.amazonaws.com/{name}'
@@ -39,22 +40,22 @@ def pageGet(page_numbre):
         offset = limit * page_numbre
 
         home_list = session.query(Home, User) \
-            .filter(Home.user_iduser == User.iduser) \
+            .filter(Home.nickname == User.nickname) \
             .order_by(Home.created_at.desc()) \
             .limit(limit).offset(offset)
 
-        next_home = session.query(Home).limit(1).offset(offset + limit)
+        next_home = session.query(Home).limit(1).offset(offset + limit).scalar()
         next_page = True if next_home else False
 
         return {
             "Homes": [{
-                "nickname": User.nickname,
-                "title": Home.title,
-                "created_at": str(Home.created_at),
-                "town": Home.town,
-                "image": Home.image,
-                "price": Home.price,
-            } for User, home in home_list],
+                "title": home.title,
+                "created_at": str(home.created_at),
+                "town": home.town,
+                "image": home.image,
+                "price": home.price,
+                "nickname": user.nickname,
+            } for home, user in home_list],
             "next_page": next_page
         }
 
@@ -62,7 +63,7 @@ def pageGet(page_numbre):
 def postGet(id):
     with session_scope() as session:
         homes = session.query(User.nickname,
-                              Home.idhoem,
+                              Home.Id_home,
                               Home.title,
                               Home.content,
                               Home.created_at,
@@ -70,11 +71,11 @@ def postGet(id):
                               Home.category,
                               Home.image,
                               Home.price
-                              ).join(User.iduser == Home.user_iduser).filter(Home.idhoem == id)
+                              ).join(User, User.nickname == Home.nickname).filter(Home.Id_home == id)
         if not home:
             abort(404, "Not Found")
         return {
-            [{
+            "Home": [{
                 "nick": nick,
                 "id": id,
                 "title": title,
@@ -85,24 +86,24 @@ def postGet(id):
                 "image": image,
                 "price": price
             } for nick, id, title, content, created_at, town, category, image, price in homes]
-        }
+        }, 200
 
 
 def homeDelete(id, nickname):
     with session_scope() as session:
 
-        users = session.query(User).filter(User.nickname == nickname)
-        if not users:
-            abort(403, "You can't modify other users' posts.")
+        del_homes = session.query(Home).filter(Home.Id_home == id, Home.nickname == nickname)
 
-        homes = session.query(Home).filter(Home.idhoem == id)
-        if not homes:
-            abort(404, "Not Found")
+        if not del_homes.scalar():
+            abort(404, 'could not find post matching this id or nickname')
 
-        session.delete(homes)
+        del_homes = del_homes.first()
+
+        s3.delete_object(Bucket=AWS_S3_BUCKET_NAME, Key=str(id))
+
+        session.delete(del_homes)
         session.commit()
 
-        s3.delete_object(Bucket=AWS_S3_BUCKET_NAME, Key=f'/temp.{id}')
         return {
                    "message": "success"
                }, 204
@@ -111,7 +112,7 @@ def homeDelete(id, nickname):
 def homePatch(title, content, town, price, nickname, id):
     with session_scope() as session:
 
-        homes = session.query(Home).filter(Home.idhoem == id)
+        homes = session.query(Home).filter(Home.Id_home == id)
 
         if not homes:
             abort(404, "Not Found")
@@ -121,7 +122,7 @@ def homePatch(title, content, town, price, nickname, id):
         if not users:
             abort(403, "You can't modify other users' posts")
 
-        name = id
+        name = str(id)
         s3_put_object(s3, AWS_S3_BUCKET_NAME, "./temp", name)
         location = s3.get_bucket_location(Bucket=AWS_S3_BUCKET_NAME)['LocationConstraint']
         image_url = f'https://{AWS_S3_BUCKET_NAME}.s3.{location}.amazonaws.com/{name}'
